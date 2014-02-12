03ada12097378c8fdc33ac1f349aa8613a6e5114
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-02-12 16:50:37 +0100

    cleanup

---
+++ test/test3.js
@@ -0,0 +1,19 @@
+var autobahn = require('./../index.js');  
+
+var connection = new autobahn.Connection({
+   url: 'ws://127.0.0.1:9000/',
+   realm: 'realm1'}
+);
+
+connection.onopen = function (session) {
+
+   session.call('com.timeservice.now').then(
+      function (now) {
+         console.log("Current time: " + now);
+         //session.leave();
+         connection.close();
+      }
+   );
+};
+
+connection.open();
